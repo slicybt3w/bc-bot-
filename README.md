1d62767e69d3cd4ae684faedd9fb0ae9435d60b6
oday549night <80214266+slicybt3w@users.noreply.github.com>
2021-06-23 08:50:09 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+bot by SL DEVELOPMENT TEAM developer 『SL』 | console.log("slicybtw");#4000
+
+copyright SL DEVELOPMENT TEAM BY 『SL』 | console.log("slicybtw");#4000
+
+dm 『SL』 | console.log("slicybtw");#5466 to get perms for using it anywhere or sharing it
+
+<img src="https://media.discordapp.net/attachments/795344675304898591/857169859837034516/20210606_141236.jpg"></img>
---
+++ website/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+</head>
+
+<body>
+<img src="https://cdn.discordapp.com/icons/788338627075375125/a_add8028cf3ff26ee0f1c21aec73a7f80.gif?size=1024"></img>
+
+<a href="https://discord.gg/BGgKbDCysk">support server</a>
+</html>
+</body>
+<?php
+$dev = "SL DEVELOPMENT";
+$creator = "『SL』 | console.log(\"slicybtw\");#4000";
+echo "copyright $dev by $creator";
+?>
